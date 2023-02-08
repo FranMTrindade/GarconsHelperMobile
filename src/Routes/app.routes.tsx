26acc5e798eb6473a68c3ebba92b9a2a9f5484a7
Francisco de Assis <contato@franciscoprog.com>
2023-02-08 17:26:51 -0300

    Rota para tela de finalizar pedido

--- src/Routes/app.routes.tsx
+++ src/Routes/app.routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
 import Order from '../pages/Order';
+import { FinishOrder } from '../pages/FinishOrder';
 
 export type StackParmsList = {
     Dashboard: undefined;
@@ -10,6 +11,11 @@ export type StackParmsList = {
         number: number | string;
         order_id: string
     };
+   
+    FinishOrder: {
+        number: number | string;
+        order_id: string
+    }
 }
 
 const Stack = createNativeStackNavigator<StackParmsList>();
@@ -17,8 +23,9 @@ const Stack = createNativeStackNavigator<StackParmsList>();
 function AppRoutes(){
     return(
         <Stack.Navigator>
-            <Stack.Screen  name="Dashboard" component={Dashboard} options={{headerShown: false}}/>
+            <Stack.Screen name="Dashboard" component={Dashboard} options={{headerShown: false}}/>
             <Stack.Screen name="Order" component={Order} options={{headerShown: false}}/>
+            <Stack.Screen name="FinishOrder" component={FinishOrder} options={{title: 'Finalizar Pedido', headerStyle:{backgroundColor: '#c0c0c0' }, headerTintColor: '#000'}} />
         </Stack.Navigator>
     )
 }
